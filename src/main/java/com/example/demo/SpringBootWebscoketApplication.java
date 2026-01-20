8c54eef1b5d98bf21a3291ebc4289ab9a464689d
ashok pawar <ashokpawarmh24@gmail.com>
2026-01-20 17:40:11 +0530

    Spring Boot WebSocket chat app

---
+++ src/main/java/com/example/demo/SpringBootWebscoketApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebscoketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebscoketApplication.class, args);
+	}
+
+}
